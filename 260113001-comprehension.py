910b162747df00fbaae665875d0d9a738fb34c4a
JrpHub1 <jrewlp@gmail.com>
2026-01-14 19:05:41 +0900

    message

--- 260113001-comprehension.py
+++ 260113001-comprehension.py
@@ -1,4 +1,13 @@
-# Comprehension
+# Comprehension 
+
+## git 계정 입력
+## touchadd, commit(conventional)
+## push origin main
+## gitignore 파일 지정
+## pre-commit.yaml 에 sample-config입력 후 설치
+## stem, main branch
+
+# Comprehension 예시 작성
 # 기존의 묶음자료형에 대해 일괄적으로 작업을 수행해 새로운 묶음자료형을 만드는 것
 
 # Before comprehension
